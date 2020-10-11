86f0d9f8c96f624196435cec66c182d5072a6fc3
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-10-11 21:00:30 +0300

    Add files via upload

---
+++ HillelHW/HW3_Milshyn/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const num1 = Math.trunc(+prompt("Insert the number:"));
+
+const num2 = Math.trunc(+prompt("Insert the number:"));
+
+const isDataValid = !isNaN(num1) && !isNaN(num2) && num2 >= 1;
+ 
+const result = Math.max(Math.round(num1 / num2) * num2, num2);
+
+!isDataValid && alert('Not valid data!');
+
+isDataValid && alert(result);
+
+
