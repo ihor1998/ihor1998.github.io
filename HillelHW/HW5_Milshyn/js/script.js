60e213b4a65d80c6b9dd6eb89a42b5999680375e
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-10-18 18:55:27 +0300

    Add files via upload

---
+++ HillelHW/HW5_Milshyn/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const login = prompt('Enter your login:')
+
+let pass = prompt('Enter your password:')
+
+if (login === 'Ihor' && pass === '1234') {
+    alert('Welcome!')
+}else if(login !== 'Ihor'){
+    alert('Incorrect login!')
+} else {
+    exit: for (pass; pass !== '1234';){
+        pass = prompt('Incorrect password!Try to enter your password again:')
+        
+        if (pass === '1234') {
+            alert('Welcome!')
+        } else if (pass !== '1234') break exit
+    }   
+
+    alert('Incorrect password!')
+}
