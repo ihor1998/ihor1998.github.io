9b6046b2b3dbacd46c5f3b96d6b9c932f248c09f
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-10-18 23:56:21 +0300

    Update script.js

--- HillelHW/HW5_Milshyn/js/script.js
+++ HillelHW/HW5_Milshyn/js/script.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const login = prompt('Enter your login:')
+const login = prompt('Enter your login:');
 
-let pass = prompt('Enter your password:')
+let pass = prompt('Enter your password:');
 
 if (login === 'Ihor' && pass === '1234') {
-    alert('Welcome!')
+    alert('Welcome!');
 }else if(login !== 'Ihor'){
-    alert('Incorrect login!')
+    alert('Incorrect login!');
 } else {
      while (pass !== '1234'){
-        pass = prompt('Incorrect password!Try to enter your password again:')
+        pass = prompt('Incorrect password!Try to enter your password again:');
         
         if (pass === '1234') {
-            alert('Welcome!')
+            alert('Welcome!');
         } else if (pass === null) break
      }   
     
-    alert('Incorrect password!')
+    alert('Incorrect password!');
 }
