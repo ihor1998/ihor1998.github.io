ffd0d79cf0bac5939fab9343b8ccd4172ec2fc2b
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-10-18 23:40:31 +0300

    Add files via upload

---
+++ HillelHW/HW6_Milshyn/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let num1 = 1;
+
+let num2 = 1;
+
+let i;
+
+const ordinalNum = +prompt('Enter the ordinal number of the Fibonacci number:');
+
+for (i = 3;i <= ordinalNum; i++) {
+    let result = num1 + num2;
+    num1 = num2;
+    num2 = result;
+}
+
+alert(num2)
+
+
+
