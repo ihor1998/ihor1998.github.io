36e1758cafaa0fb88a3309eb119e5b9383d28a30
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-11-03 02:41:57 +0200

    Add files via upload

---
+++ HillelHW/HW11_Milshyn/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const obj = {
+    '0': "zero",
+    '42': "answer",
+    'greeting': "Hello",
+    '3.14': "PI",
+};
+
+
+function getNumKey(obj) {
+
+    for (let key in obj) {
+       
+        if (!isNaN(key)) {
+            console.log(obj[key]); 
+        };
+    };
+
+};
+
+getNumKey(obj);
