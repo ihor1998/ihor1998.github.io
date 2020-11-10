f1b2f16aa6684b0edcc58f94219acd95ff9375b4
ihor1998 <71885292+ihor1998@users.noreply.github.com>
2020-11-10 20:03:07 +0200

    Add files via upload

---
+++ HillelHW/HW13_Milshyn/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function Cat(name, breed) {
+    this.name = name;
+    this.breed = breed;
+    
+    this.meow = function () {
+        alert('Meow!')
+    };
+}
+
+function User(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+   
+    this.greet = function () {
+        alert(`Hi! ${this.firstName})`);
+    }
+}
+
